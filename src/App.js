e7d82d4990df09ecdbf22b4987e9fff5ddce491b
Wagar1 <vuqar.eyvazov.1997@gmail.com>
2021-10-06 16:28:04 +0400

    page 50

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
+import { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const list = [ { title: 'React', url: 'https://facebook.github.io/react/', author: 'Jordan Walke', num_comments: 3, points: 4, objectID: 0, }, { title: 'Redux', url: 'https://github.com/reactjs/redux', author: 'Dan Abramov, Andrew Clark', num_comments: 2, points: 5, objectID: 1, }, ];
+
+class App extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      list
+    }
+
+    this.onDismiss = this.onDismiss.bind(this);
+  }
+
+  onDismiss(id){
+    const isNotId = item => item.objectID !== id; 
+    const updatedList = this.state.list.filter(isNotId);
+    this.setState({ list: updatedList });
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <form> 
+          <input type="text" /> 
+        </form>
+        <h2>
+          {
+            this.state.list.map((item) => {
+              const onHandleDismiss = () => this.onDismiss(item.objectID);
+              return(
+                <div key={item.objectID}>
+                  <span>
+                    <a href={item.url}>{item.title}</a>
+                  </span>&nbsp;
+                  <span>{item.author}</span>
+                  <span>{item.num_comments}</span>
+                  <span>{item.points}</span>
+                  <span>
+                    <button
+                      onClick={onHandleDismiss}
+                      type="button"
+                    >Dismiss</button>
+                  </span>
+                </div>
+              )
+            })
+          }
+        </h2> 
     </div>
-  );
+    );
+  }
 }
 
 export default App;
