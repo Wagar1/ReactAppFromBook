814e15479de497e6450186e03472a00ed1b9ce39
Wagar1 <vuqar.eyvazov.1997@gmail.com>
2021-10-07 09:56:04 +0400

    search an item

--- src/App.js
+++ src/App.js
@@ -3,15 +3,22 @@ import './App.css';
 
 const list = [ { title: 'React', url: 'https://facebook.github.io/react/', author: 'Jordan Walke', num_comments: 3, points: 4, objectID: 0, }, { title: 'Redux', url: 'https://github.com/reactjs/redux', author: 'Dan Abramov, Andrew Clark', num_comments: 2, points: 5, objectID: 1, }, ];
 
+const isSearched = searchTerm => item => item.title.toLowerCase().includes(searchTerm.toLowerCase());
 class App extends Component {
 
   constructor(props){
     super(props);
     this.state = {
-      list
+      list,
+      searchTerm: '',
     }
 
+    this.onSearchChange = this.onSearchChange.bind(this);
     this.onDismiss = this.onDismiss.bind(this);
+  }
+
+  onSearchChange(event){
+    this.setState({ searchTerm: event.target.value });
   }
 
   onDismiss(id){
@@ -21,14 +28,17 @@ class App extends Component {
   }
 
   render(){
+    const { searchTerm, list } = this.state;
     return (
       <div className="App">
         <form> 
-          <input type="text" /> 
+          <input 
+              type = "text"
+              onChange = {this.onSearchChange} /> 
         </form>
         <h2>
           {
-            this.state.list.map((item) => {
+            list.filter(isSearched(searchTerm)).map((item) => {
               const onHandleDismiss = () => this.onDismiss(item.objectID);
               return(
                 <div key={item.objectID}>
