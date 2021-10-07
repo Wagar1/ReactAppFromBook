1e5438e69c50e74ff5080d9183aea01e7eeeb154
Wagar1 <vuqar.eyvazov.1997@gmail.com>
2021-10-07 12:35:00 +0400

    Spread operator in dismiss method

--- src/App.js
+++ src/App.js
@@ -35,7 +35,7 @@ class App extends Component {
   onDismiss(id){
     const isNotId = item => item.objectID !== id; 
     const updatedHits = this.state.result.hits.filter(isNotId);
-    this.setState({ result: Object.assign({}, this.state.result, { hits: updatedHits })});
+    this.setState({ result: { ...this.state.result, hits: updatedHits } });
   }
 
   componentDidMount() { 
